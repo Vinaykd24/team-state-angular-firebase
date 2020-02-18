e1cd66864b1ac5522a59de1693a5a1ac780208f5
Vinay Kadam <vinaykd@Vinays-MacBook-Pro.local>
2020-02-17 21:40:49 -0500

    Added angular firebase and flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,41 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./maretial.module";
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { PlayerComponent } from './player/player.component';
-import { AddPlayerComponent } from './player/add-player/add-player.component';
-import { MatchComponent } from './match/match.component';
-import { AddMatchComponent } from './match/add-match/add-match.component';
-import { TournamentComponent } from './tournament/tournament.component';
-import { AddTournamentComponent } from './tournament/add-tournament/add-tournament.component';
-import { AddMatchDetailComponent } from './match/add-match-detail/add-match-detail.component';
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { PlayerComponent } from "./player/player.component";
+import { AddPlayerComponent } from "./player/add-player/add-player.component";
+import { MatchComponent } from "./match/match.component";
+import { AddMatchComponent } from "./match/add-match/add-match.component";
+import { TournamentComponent } from "./tournament/tournament.component";
+import { AddTournamentComponent } from "./tournament/add-tournament/add-tournament.component";
+import { AddMatchDetailComponent } from "./match/add-match-detail/add-match-detail.component";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent, LoginComponent, PlayerComponent, AddPlayerComponent, MatchComponent, AddMatchComponent, TournamentComponent, AddTournamentComponent, AddMatchDetailComponent],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    PlayerComponent,
+    AddPlayerComponent,
+    MatchComponent,
+    AddMatchComponent,
+    TournamentComponent,
+    AddTournamentComponent,
+    AddMatchDetailComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+const firebaseConfig = {
+  apiKey: "AIzaSyAYSr_Fi81UcrhloHYK8mBFoW03xIrodIo",
+  authDomain: "teamstats-5b227.firebaseapp.com",
+  databaseURL: "https://teamstats-5b227.firebaseio.com",
+  projectId: "teamstats-5b227",
+  storageBucket: "teamstats-5b227.appspot.com",
+  messagingSenderId: "798479054512",
+  appId: "1:798479054512:web:7f53f0b0d1275649f1ad75"
 };
-
+export const environment = {
+  production: false,
+  firebase: firebaseConfig
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
