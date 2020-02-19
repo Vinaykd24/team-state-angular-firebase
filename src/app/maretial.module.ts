bf5fd4eb583c99a6723a6a1ebfac23b1f2472c91
Vinay Kadam <vinaykd@Vinays-MacBook-Pro.local>
2020-02-18 23:33:43 -0500

    Added sidenav and toolbar

--- src/app/maretial.module.ts
+++ src/app/maretial.module.ts
@@ -2,9 +2,26 @@ import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
-  exports: [MatFormFieldModule, MatInputModule, MatButtonModule]
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
+  ]
 })
 export class MaterialModule {}
