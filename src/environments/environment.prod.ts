dc69c09bb2737c49822076a653f78ef470753e46
Vinay Kadam <vinaykd@Vinays-MacBook-Pro.local>
2020-03-25 16:22:54 -0400

    Updated prod environment ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYSr_Fi81UcrhloHYK8mBFoW03xIrodIo",
+  authDomain: "teamstats-5b227.firebaseapp.com",
+  databaseURL: "https://teamstats-5b227.firebaseio.com",
+  projectId: "teamstats-5b227",
+  storageBucket: "teamstats-5b227.appspot.com",
+  messagingSenderId: "798479054512",
+  appId: "1:798479054512:web:7f53f0b0d1275649f1ad75"
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: firebaseConfig
 };
