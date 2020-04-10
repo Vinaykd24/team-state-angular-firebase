43588ece33d7fdea7fc7ca199cd5f1445c2ff893
Vinay Kadam <vinaykd@Vinays-MacBook-Pro.local>
2020-04-10 16:24:58 -0400

    Added season entry

--- src/app/tournament/add-tournament/add-tournament.component.ts
+++ src/app/tournament/add-tournament/add-tournament.component.ts
@@ -6,9 +6,18 @@ import { Router } from "@angular/router";
 @Component({
   selector: "app-add-tournament",
   templateUrl: "./add-tournament.component.html",
-  styleUrls: ["./add-tournament.component.css"]
+  styleUrls: ["./add-tournament.component.css"],
 })
 export class AddTournamentComponent implements OnInit {
+  seasons = [
+    { id: "1", value: "2019-20" },
+    { id: "2", value: "2018-19" },
+    { id: "3", value: "2017-18" },
+    { id: "4", value: "2016-17" },
+    { id: "5", value: "2015-16" },
+    { id: "6", value: "2014-15" },
+    { id: "7", value: "2013-14" },
+  ];
   constructor(
     private tournamentService: TournamentService,
     private router: Router
